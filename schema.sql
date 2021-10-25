f5b087e1396e101a409824145ce2f63c9be92b06
Andres Castañeda <af.castaneda223@gmail.com>
2021-10-25 15:36:17 -0300

    add indexes to improve queries

--- schema.sql
+++ schema.sql
@@ -58,3 +58,6 @@ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id F
 -- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
+CREATE INDEX visits_animals_desc ON visits(animal_id DESC);
+CREATE INDEX visits_vet_id_desc ON visits(vet_id DESC); 
+CREATE INDEX owners_email ON owners(email ASC);
